2ea971d1c7522ab0de48ea34de143823ae2156f6
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-09 11:24:11 -0700

    more of hello c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Size of char: %d byte(s)", (int) sizeof(char));
+	printf("Size of int: %d byte(s)", (int) sizeof(int));
+	printf("Size of long int: %d byte(s)", (int) sizeof(long int));
+	printf("Size of long long int: %d byte(s)", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)", (int) sizeof(float));
+
+}
