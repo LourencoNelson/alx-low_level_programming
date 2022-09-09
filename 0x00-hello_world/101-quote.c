64d8462d53f93bac5df210e692a44e706adfed97
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-09 13:00:08 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@
  */
 int main(void)
 {
-	fprinf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
 
