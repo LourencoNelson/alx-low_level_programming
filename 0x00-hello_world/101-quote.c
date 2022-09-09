573cf3cc2585f663d7dae1c7dee359891f0fb791
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-09 12:53:59 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *fprintf - Output to the standard error
+ *
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	fprinf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
+
