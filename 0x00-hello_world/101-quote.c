b51e0572e42dc46f81f3fe9d9ec929f81c2a778d
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-09 20:17:17 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  *main - Entry point
@@ -13,5 +10,5 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
