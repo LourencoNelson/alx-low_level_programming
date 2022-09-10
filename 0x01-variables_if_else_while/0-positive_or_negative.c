9e03e87ad68647f7abd905a792086a8a82e91a5c
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-09 21:52:03 -0700

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
 *main - Entry point
 *
-*printf - prints the result of a test if 
+*printf - prints the result of a test if
 *
 *Return: always 0 (Success)
 */
@@ -18,7 +18,7 @@ int main(void)
 	
 	if (n < 0)
 	{
-		printf("%d is negative\n", n);	
+		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
 	{
