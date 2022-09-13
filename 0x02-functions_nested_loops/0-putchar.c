a683dec6b549514b9754e0e2ecd3202c8764f7bd
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-12 23:53:24 -0700

    c functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *print - prints to the screen
+ *
+ *Return - always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
