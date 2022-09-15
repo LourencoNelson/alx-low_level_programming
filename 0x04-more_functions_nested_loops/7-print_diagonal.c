43a9cb6d54f8dd84aadb1c8e176bd1c2db6eae17
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-15 11:46:31 -0700

    more functions nested loops task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @n: width of the line
+ *
+ * _putchar - prints the line to the screen
+ *
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
