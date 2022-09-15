12ac251f3ad767ea26cb4c53d5a23af4f9122a9a
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-15 09:04:53 -0700

    more functions nested loops task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include  <ctype.h>
+
+/**
+ * _isupper - checks for a digit
+ * @c: input character
+ *
+ * Return: 1 if is a digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int ret;
+
+	if (isdigit(c))
+		ret = 1;
+	else
+		ret = 0;
+
+	return (ret);
+}
