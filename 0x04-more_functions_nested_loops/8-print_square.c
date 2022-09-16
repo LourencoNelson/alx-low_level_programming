9b768412346b615305073a39788f2829ae02352b
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-15 19:58:10 -0700

    more functions nested loops task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,12 @@
 void print_square(int size)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < size; i++)
 	{
-		_putchar('#');
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
