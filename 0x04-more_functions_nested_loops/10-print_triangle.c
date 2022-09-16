f075d5bfa0ef0e32d662d1b7e33cc193f98d498b
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-16 02:59:44 -0700

    more functions nested loops task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,18 +12,15 @@ void print_triangle(int size)
 	int i;
 	int j;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('#');
+		_putchar('\n');
+	}
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < i; j++)
-				_putchar(' ');
-			_putchar('#');
-			_putchar('\n');
-		}
 	}
 }
