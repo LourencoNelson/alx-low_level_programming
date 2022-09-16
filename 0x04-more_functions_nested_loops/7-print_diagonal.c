b7151fc41370b73bb90ab603c6310bcba220162b
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-16 02:54:12 -0700

    more functions nested loops task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,15 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
-	{
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 1; j <= i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
 	}
 }
