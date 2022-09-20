14282338c8eb8dc2065ea9b0ebbb79b28748d7f3
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-19 18:47:16 -0700

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse
+ * @s: string
+ *
+ * putchar - prints to the screen
+ *
+ */
+void rev_string(char *s)
+{
+	int c = 0;
+	int i = 0;
+	char ch = *(s + c);
+
+	if (*(s + c) != '\0')
+	{
+		while (1)
+		{
+			if (*(s + c) != '\0')
+			{
+				c++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		do {
+			c--;
+			&(s + i) = *(s + c);
+			i++;
+		} while (*(s + c) != ch);
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *c);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
 
