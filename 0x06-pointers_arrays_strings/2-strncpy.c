a59077ca5e4e91d385376dd65d79a3b8fa7ff072
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-09-22 14:13:40 -0700

    more pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: destination
+ * @src: source
+ * @n: limit
+ *
+ * Return: desttination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	char *ptr = dest;
+
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
