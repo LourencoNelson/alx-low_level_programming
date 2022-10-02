5c9fda8cda8f58f2a203b6c8f5430650825f1b0d
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-02 03:02:52 -0700

    argc and argv[] tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point of a C program
  * @argc: Number of elements of argv[]
  * @argv: arguments of the command line
  *
- * print f - prints its content to the stdo
+ * _putchar - prints its content to the stdo
  *
  * Return: always 0 (Succes)
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", (argc - 1));
+	_putchar(arhc + '0');
+	_putchar('\n');
 	return (0);
 }
