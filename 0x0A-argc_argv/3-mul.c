36d4177da484e36d1c8ddd570dae26f84550f929
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-03 03:26:46 -0700

    argc and argv[] tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point of a C program
+ * @argc: Number of elements of argv[]
+ * @argv: arguments of the command line
+ *
+ * printf - prints its content to the stdo
+ *
+ * Return: always 0 (Succes)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		int r = a * b;
+
+		printf("%d\n", r);
+		return (0);
+	}
+}
