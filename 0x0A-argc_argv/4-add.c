9d5ea245a15f60d7b9e7eb35af0e7e05965bdeaf
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-03 09:26:35 -0700

    argc and argv[] tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
 	}
 	else
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 0; i < argc; i++)
 		{
 			if (atoi(argv[i]))
 			{
