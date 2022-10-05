306f0a9ad79b948cd0fc4a0290a3027b05043686
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-05 16:22:47 -0700

    malloc and free tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,10 +23,7 @@ char *create_array(unsigned int size, char c)
 		for (i = 0; i < size; i++)
 			array[i] = c;
 
-		if (array == NULL)
-			return (NULL);
-		else
-			return (array);
+		return (array);
 
 	}
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strdup - string copy
+ * @str: string to copy
+ *
+ * Return: a pointer to the new string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			i++;
+		}
+
+		s = (char *)malloc(sizeof(char) * i);
+
+		for (j = 0; j < i; j++)
+		{
+			s[j] = str[j];
+		}
+		s[j] = '\0';
+	}
+	return (s);
+}
