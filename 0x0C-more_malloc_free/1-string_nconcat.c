03775e2a64fe6594efee246b5705cbfb07085cb4
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-06 05:49:08 -0700

    more malloc tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,58 @@
 #inlcude "main.h"
-
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: just a var
+ * @n: aux variable
  *
  * Return: pointer to a string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, len1, ln2, totlen;
+	int i, j, len1, ln2, totlen;
 	char *s;
 
+	if (s1 == NULL)
+		s1[0] = '\0';
+	if (s2 == NULL)
+		s2[0] = '\0';
 	for (ln1 = 0; s1[ln1] != '\0'; ln1++)
 		;
 	for (ln2 = 0; s2[ln2] != '\0'; ln2++)
 		;
 	totlen = ln1 + ln2;
-
-	if (n >= len2)
+	s = malloc(totlen + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0, j = 0; i < totlen; i++)
 	{
-		s = malloc(len1 + len2 + 1);
-		if (s == NULL)
-			return (NULL);
-		for (i = 0; i < totlen; i++)
+		if (n >= ln2)
 		{
-			if (i < len1)
+			if (i < ln1)
+			{
 				s[i] = s1[i];
+			}
 			else
-				s[i] = s2[i];
+			{
+				s[i] = s2[j];
+				j++;
+			}
 		}
-		s[i] = '\0';
-	}
-	else
-	{
-		s = malloc(ln1 + ln2 + 1);
-		if (s == NULL)
-			return (NULL);
-		for (i = 0; i < len1 + n; i++)
+		else
 		{
-			if (i < len1)
+			if (i < ln1)
+			{
 				s[i] = s1[i];
+			}
 			else
-				s[i] = s2[i];
+			{
+				s[i] = s2[j];
+				j++;
+			}
+			if (j > n)
+				break;
 		}
-		s[i] = '\0';
 	}
+	s[i] = '\0';
 	return (s);
 }
