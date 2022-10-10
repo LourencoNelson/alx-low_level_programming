67b1316e1f4e9fc34d2cd0a42b8217caba73fb20
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-10 09:39:52 -0700

    struct and typedef tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for dogs
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: age of the dog's owner
+ *
+ * Description: The structure creates a type of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
