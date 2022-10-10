024d6ee8cf52398a8edbd2d356500faf84ca20f4
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-10 09:45:16 -0700

    struct and typedef tasks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	struct dog my_dog;
+
+	my_dog.name = d->name;
+	my_dog.age = d->age;
+	my_dog.owner = d->owner;
 }
