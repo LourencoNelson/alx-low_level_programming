1736566131dcefbc828748a402b905106682228d
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-12 15:26:38 -0700

    function pointers tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name parameter
+ * @f: callback
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
