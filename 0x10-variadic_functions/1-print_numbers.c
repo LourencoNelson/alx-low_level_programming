27a6551fe277e410a67fb514d312af4ee17874bc
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-13 08:40:00 -0700

    variadic functions tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: numbers separator
+ * @n: number of args
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list ap;
+
+	va_start(a, n);
+
+	for (i = 0; i < (int) n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%d ", va_arg(ap, int));
+		}
+		else
+		{
+			printf("%d", va_arg(ap, int));
+			if (i != (int) n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
