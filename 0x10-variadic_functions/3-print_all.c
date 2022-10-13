77f0d0c0bb0f9dda126c8fca06283f8bf8819607
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-13 10:52:01 -0700

    variadic functions tasks

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints all
+ * @format: string formater
+ */
+void print_all(const char * const format, ...)
+{
+	int size, j, i, c, flag;
+	double f;
+	char *s;
+	va_list ap;
+
+	flag = 0;
+	size = strlen(format);
+	va_start(ap, format);
+	j = 0;
+
+	while (j < size)
+	{
+		switch (format[j])
+		{
+			case 'c':
+				c = va_arg(ap, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(ap, int);
+				printf("%i", i);
+				break;
+			case 'f':
+				f = va_arg(ap, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(ap, char*);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			default:
+				flag = -1;
+				break;
+		}
+		if (j != (int) size - 1 && flag == 0)
+		{
+			printf(", ");
+		}
+		if (flag == -1)
+			flag = 0;
+		j++;
+	}
+	printf("\n");
+	va_end(ap);
+}
