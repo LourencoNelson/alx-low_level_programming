da0b2fb4ea4e35729918d57d0d9fbdda3b2dff0c
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-13 05:58:51 -0700

    variadic functions tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all of its args
+ * @n: number of args
+ *
+ * Return: sum of all args (Success)
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
