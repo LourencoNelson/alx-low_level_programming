c3f6cb72fc67e88bc9bac24e00107c6909fa5c68
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-23 05:56:14 -0700

    more pointers, array and string tasks

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - set memory with a constant byte
+ * @s: pointer
+ * @b: byte
+ * @n: number of bytes
+ *
+ * Return: memory pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + 1) = b;
+	}
+
+	return (s);
+}
