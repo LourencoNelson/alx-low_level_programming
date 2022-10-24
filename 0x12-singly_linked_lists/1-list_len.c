c2bb73c5817ee66908849e9e4dc5b2248796092c
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 10:07:28 -0700

    linked list tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements of a linked list
+ * @h: head of the list
+ *
+ * Return: length of the list;
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int count;
+
+	count = 0;
+
+	while (temp != NULL)
+		count++;
+
+	return (count);
+}
