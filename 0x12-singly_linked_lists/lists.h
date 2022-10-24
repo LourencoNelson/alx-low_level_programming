8bed66d151cf3b9489aa2f487350ea7a33abc596
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 09:43:44 -0700

    linked list tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_t - linked list node
+ * @str: node data
+ * @len: node position
+ * @next: next node pointer
+ *
+ * Description: structure for a node of a linked list
+ */
+struct list_t
+{
+	char *str;
+	int len;
+	struct list_t *next;
+};
+
+/**
+ * list_t - Typedef for a list node
+ */
+typedef struct list_t list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
