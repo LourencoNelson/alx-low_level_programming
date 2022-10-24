327d2455a0599d59e093b989f816dd880cb537f3
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 13:47:35 -0700

    linked list tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,30 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new element at the start of the list
+ * add_node_end - adds a new element at the end of the list
  * @head: head of the list
  * @str: string data
  *
  * Return: address of the new element;
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
+	list_t **temp;
 
+	*temp = *head;
 	new = (list_t *) malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	while ((*temp)->next != NULL)
+	{
+		*temp = (*temp)->next;
+	}
+
 	new->str = strdup(str);
-	new->next = *head;
-	*head = new;
+	(*temp)->next = new;
 
 	return (new);
 }
