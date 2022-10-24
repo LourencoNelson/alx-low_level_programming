0efd4c94728c352282887af18e24ee3c59676d58
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 10:10:09 -0700

    linked list tasks

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,11 +9,16 @@
 size_t list_len(const list_t *h)
 {
 	unsigned int count;
+	list_t *temp;
 
+	temp = (list_t *) h;
 	count = 0;
 
 	while (temp != NULL)
+	{
+		temp = temp->next;
 		count++;
+	}
 
 	return (count);
 }
