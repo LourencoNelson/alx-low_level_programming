2e9e3e79e511d418b483d98edf7bbd5d0c4e9d93
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 12:01:11 -0700

    linked list tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,13 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = (list_t *) malloc(sizeof(list_t));
+	
+	if (new == NULL)
+		return (NULL);
 
 	new->str = strdup(str);
 	new->next = *head;
-
 	*head = new;
-
 
 	return (new);
 }
