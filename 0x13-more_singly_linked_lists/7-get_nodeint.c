dc2e3954d5db97d2356a8e52abf7b32cd9603b5b
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 22:50:25 -0700

    more linked list tasks

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,11 +17,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (tmp != NULL)
 	{
-		if (count == index)
-		{
-			return (tmp);
-		}
 		tmp = tmp->next;
 		count++;
+		if (count == (index - 1))
+			return (tmp);
 	}
 }
