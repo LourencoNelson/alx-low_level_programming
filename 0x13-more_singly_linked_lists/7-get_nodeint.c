88bd54bf0fe6baa158b5d7e904e45d50257a0ca1
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-24 22:39:34 -0700

    more linked list tasks

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - gets a node in the list
+ * @head: head of the list
+ * @index: index of the node
+ *
+ * Return: the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int count;
+
+	tmp = head;
+	count = 0;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		count++;
+
+		if (count == (index -1))
+			break;
+	}
+
+	return (tmp);
+
+}
