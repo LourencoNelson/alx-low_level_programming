32fc429bf010f92ab0b5c022fda85696ab5bf689
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-25 03:32:43 -0700

    0x02 tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,15 @@
  */
 int print_last_digit(int n)
 {
-	int l;
+	int l, num;
 
 	if (n < 0)
-		n = n * (-1);
+		num = n * (-1);
+	else
+		num = n;
+
 	l = n % 10;
+
 	_putchar(l + '0');
 
 	return (l);
