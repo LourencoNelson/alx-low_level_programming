4b997b6138574794b77d6552b5465f48cad35bc9
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-25 04:10:24 -0700

    function pointers tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < (unsigned  int) size; i++)
+	for (i = 0; i < (unsigned int) size; i++)
 	{
 		(*action)(array[i]);
 	}
