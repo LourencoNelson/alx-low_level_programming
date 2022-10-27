f97049b07f55179e1b5cb7bbe74beb882f83fe08
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-27 03:09:04 -0700

    struct and typedef tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,9 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	(*d).name = name;
+	strcpy(d->name, name);
 	(*d).age = age;
-	(*d).owner = owner;
+	strcpy(d->owner, owner);
 
 	return (d);
 }
