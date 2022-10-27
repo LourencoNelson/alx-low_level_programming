a8191fd07be5eccdb0feec32133a314b634ef8fa
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-10-26 19:19:44 -0700

    more linked list tasks

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - adds a new node to a list
+ * @head: head of the list
+ * @index: position to insert
+ *
+ * Return: the address of the new node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, del;
+	int count, i;
+
+	count = 0;
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+
+	if (count < index || index < 1)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		temp = *head;
+		head = head->next;
+		temp->next = NULL;
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		temp = *head;
+		for (i = 0; i < index - 1; i++)
+		{
+			temp = temp->next;
+		}
+
+		del = temp->next;
+		del->next = NULL;
+		free(del);
+		return (1);
+	}
+	return (-1);
+}
