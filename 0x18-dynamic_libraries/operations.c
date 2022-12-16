12a1f8c0618bd491ef19239f37df8591f395cc51
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-12-16 14:06:15 +0200

    dynamic lib advanced task 100

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - Calculates the sum of two numbers
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Calculates the difference of two numbers
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Calculates the product of two numbers
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Calculates the division
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The division of a with b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - calculates the remainder
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The remaider of a divided with b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
