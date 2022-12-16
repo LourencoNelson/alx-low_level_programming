905a5231772d1f153e2c0f8b6a5e5f057605d660
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-12-16 19:37:37 +0200

    double linked lists task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - inserts a node at the beginning
+ * @head: pointer of the head
+ * @n: data to insert
+ *
+ * Return: address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *new;
+	size_t size = 0;
+
+	temp = (dlistint_t *) *head;
+
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (temp == NULL)
+	{
+		temp = new;
+	}
+	else
+	{
+		new->next = temp;
+		temp->prev = new;
+		temp = new;
+	}
+	return (new);
+}
