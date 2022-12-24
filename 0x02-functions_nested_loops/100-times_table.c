978dfca386194d843103983f0bb8e7803018fddb
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-12-24 16:03:37 +0200

    0x02 tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n time table
+ * @n: argument
+ *
+ * Return: void
+ */
 void print_times_table(int n)
 {
 	int i;
 	int j;
 
-	if ((n >= 0) || (n <= 15))
+	if ((n >= 0) && (n <= 15))
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				printf(i*j);
-				if(j != n)
+				printf("%d", (i * j));
+				if (j != n)
 				{
 					printf(", ");
 				}
