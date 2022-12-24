2fbbe2353f83cf0022a9caec0b2f7811a1d88a52
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-12-24 23:20:16 +0200

    more functions nested loops task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - prints numbers
+ * @n: input number
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i, j count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i  = -n;
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(n / count % 10 + '0');
+	}
+}
