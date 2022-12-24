87d0696c88dc93ddf636684e8556ceeecad17bdc
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2022-12-24 20:06:45 +0200

    0x02 tasks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - prints
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	long int a = 0;
+	long int b = 1;
+	long int sum = 0;
+
+	long int fib = a + b;
+
+	while (fib <= 40000000)
+	{
+		if (fib % 2 == 0)
+			sum += fib;
+		a = b;
+		b = fib;
+		fib = a + b;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
