241522751db228883cb291bb2d802dffe3c73c9d
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-06-12 21:51:00 +0200

    malloc tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,14 +9,30 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j;
 	int **array = NULL;
 
+	if (width <= 0 ||  height <= 0)
+		return (NULL);
+
 	array = (int **)malloc(width * sizeof(int *));
+
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
 		array[i] = (int *)malloc(height * sizeof(int));
+		if (array[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j++ < height; j++)
+		{
+			array[i][j] = 0;
+		}
 	}
 
 	return (array);
