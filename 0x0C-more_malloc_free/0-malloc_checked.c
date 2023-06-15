20513600eb47f6ce2cfe7b3629c7a5e687821d8a
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-06-15 20:17:41 +0200

    more malloc tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b:number of bytes
+ *
+ * Return: pointer (Success)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer	= malloc(b);
+
+	if (pointer == NULL)
+		exit(98);
+	
+	return (pointer);
+}
