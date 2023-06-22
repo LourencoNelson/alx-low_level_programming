0e6505fd4e323ba722a3c29664a7239052fa1e20
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-06-22 18:42:14 +0200

    variadic tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints a variable number of args
+ * @separator: separator
+ * @n: number of args
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num = 0;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+
+		if ( i == (n - 1))
+			break;
+
+		if (separator != NULL)
+			printf("%d%s", num, separator);
+		else
+			printf("%d", num);
+	}
+	printf("%d\n", num);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+/**
+ *  print_strings - prints a variable number of args
+ * @separator: separator
+ * @n: number of args
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str = NULL;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char*);
+
+		if (i == (n - 1))
+			break;
+
+		if (separator != NULL)
+		{
+			if (str == NULL)
+				printf("(nil)%s", separator);
+			else
+				printf("%s%s", str, separator);
+		}
+		else
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+	}
+	if (str == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", str);
+}
