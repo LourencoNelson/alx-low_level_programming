50afda7283b8a15f10f1f92c228164d1622aca00
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-06-30 19:29:04 +0200

    linked list tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,12 +11,18 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp;
+	unsigned int len = 0;
 
 	temp = *head;
-	new = (list_t *) malloc(sizeof(list_t));
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -24,10 +30,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
-	while (temp->next != NULL)
-	{
+
+	while (temp->next)
 		temp = temp->next;
-	}
 
 	temp->next = new;
 
