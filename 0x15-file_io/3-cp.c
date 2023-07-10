b4230bf8cc05d0c56f066418f469bce09a359192
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-07-11 00:37:46 +0200

    file io tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ char *create_buffer(char *file)
 
 	buffer = malloc(sizeof(char) * 1024);
 
-	if (bufer == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", file);
