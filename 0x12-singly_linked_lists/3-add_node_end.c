bcead4cf4fd1725cb9c9b2bcec5be1d001cc40b0
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2024-02-01 22:53:46 +0200

    all tasks

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new element at the end of the list
+ * @head: head of the list
+ * @str: string data
+ *
+ * Return: address of the new element;
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp;
+	unsigned int len = 0;
+
+	temp = *head;
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,35 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_t - linked list node
+ * @str: node data
+ * @len: node position
+ * @next: next node pointer
+ *
+ * Description: structure for a node of a linked list
+ */
+struct list_t
+{
+	char *str;
+	int len;
+	struct list_t *next;
+};
+
+/**
+ * list_t - Typedef for a list node
+ */
+typedef struct list_t list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
